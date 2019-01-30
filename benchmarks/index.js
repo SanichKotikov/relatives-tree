541777323dff6c7e2369f7265e981096157d92db
Sanich <sancho@cdrpro.ru>
2019-01-30 10:28:23 +0700

    Added benchmarks

---
+++ benchmarks/index.js
@@ -0,0 +1,17 @@
+const benchmark = require('benchmark');
+const getFamilyTree = require('../lib/index').default;
+const sample = require('./sample.json');
+
+const DEFAULT_ROOT_ID = 'kuVISwh7w';
+const SIMPLE_ROOT_ID = 'vRSjcaDGj';
+
+function run(nodes) {
+  (new benchmark.Suite('relatives-tree benchmark'))
+    .add('Default', function() { getFamilyTree(nodes, DEFAULT_ROOT_ID); })
+    .add('Simple', function() { getFamilyTree(nodes, SIMPLE_ROOT_ID); })
+    .on('cycle', (event) => console.log('> ' + String(event.target)))
+    .on('error', (event) => console.log(event.target.error.message))
+    .run();
+}
+
+run(sample);
