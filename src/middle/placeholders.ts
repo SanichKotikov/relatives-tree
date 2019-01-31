e24ef5f3f3fc7ff7ae0df4e0f50ce99c2e134fc5
Sanich <sancho@cdrpro.ru>
2019-01-31 20:00:43 +0700

    Refactored placeholders

--- src/middle/placeholders.ts
+++ src/middle/placeholders.ts
@@ -1,52 +1,40 @@
 import Store from '../store';
-import { IFamilyNode } from '../types';
+import { Gender, RelationType, IRelation, IFamilyNode } from '../types';
 
-// TODO:
+const createRel = (id: string, type: RelationType = 'blood'): IRelation => ({ id, type });
+
+const createNode = (gender: Gender): IFamilyNode => ({
+  id: `${gender}-placeholder`,
+  placeholder: true,
+  gender: gender,
+  parents: [],
+  siblings: [],
+  spouses: [],
+  children: [],
+});
+
+const createParents = (store: Store, root: IFamilyNode): IRelation[] => {
+  const father = createNode('male');
+  const mother = createNode('female');
+
+  father.spouses = [createRel(mother.id, 'married')];
+  mother.spouses = [createRel(father.id, 'married')];
+
+  return [father, mother].map(node => {
+    node.children = root.siblings.concat(createRel(root.id));
+    store.nodes.set(node.id, node);
+
+    return createRel(node.id);
+  });
+};
+
 export default (store: Store): Store => {
   if (store.rootNode.parents.length) return store;
+  const root = store.rootNode;
+  const parents = createParents(store, root);
 
-  const father: IFamilyNode = {
-    id: 'father-placeholder',
-    placeholder: true,
-    gender: 'male',
-    parents: [],
-    siblings: [],
-    spouses: [
-      { id: 'mother-placeholder', type: 'married' },
-    ],
-    children: [
-      { id: store.rootNode.id, type: 'blood' },
-      ...store.rootNode.siblings,
-    ],
-  };
-
-  const mother: IFamilyNode = {
-    id: 'mother-placeholder',
-    placeholder: true,
-    gender: 'female',
-    parents: [],
-    siblings: [],
-    spouses: [
-      { id: 'father-placeholder', type: 'married' },
-    ],
-    children: [
-      { id: store.rootNode.id, type: 'blood' },
-      ...store.rootNode.siblings,
-    ],
-  };
-
-  store.rootNode.parents = [
-    { id: father.id, type: 'blood' },
-    { id: mother.id, type: 'blood' },
-  ];
-
-  store.rootNode.siblings.forEach(rel => {
-    const sibling = store.getNode(rel.id);
-    sibling.parents = store.rootNode.parents;
-  });
-
-  store.nodes.set(father.id, father);
-  store.nodes.set(mother.id, mother);
+  root.parents = parents.slice();
+  root.siblings.forEach(rel => store.getNode(rel.id).parents = parents.slice());
 
   return store;
 };
