29287094980753733e8f450f98a9e2a353e15c00
better-think <web.solutions9218@gmail.com>
2023-09-28 01:36:31 +0800

    Update  types.ts
    extend Node type for typescript

--- src/types.ts
+++ src/types.ts
@@ -1,87 +1,80 @@
-export type Mutable<T> = {
-  -readonly [P in keyof T]: T[P];
-};
-
-export const enum Gender {
-  male = 'male',
-  female = 'female',
 }
-
+ 
 export const enum RelType {
-  blood = 'blood',
-  married = 'married',
-  divorced = 'divorced',
-  adopted = 'adopted',
-  half = 'half',
+ blood = 'blood',
+ married = 'married',
+ divorced = 'divorced',
+ adopted = 'adopted',
+ half = 'half',
 }
-
+ 
 export const enum FamilyType {
-  root = 'root',
-  child = 'child',
-  parent = 'parent',
+ root = 'root',
+ child = 'child',
+ parent = 'parent',
 }
-
+ 
 export type Family = {
-  readonly id: number;
-  readonly type: FamilyType;
-  readonly main: boolean;
-  /** Parent family ID */
-  pid?: number;
-  /** Child family ID */
-  cid?: number;
-  /** Family's left coordinate */
-  X: number;
-  /** Family's top coordinate */
-  Y: number;
-  parents: readonly Unit[];
-  children: readonly Unit[];
+ readonly id: number;
+ readonly type: FamilyType;
+ readonly main: boolean;
+ /** Parent family ID */
+ pid?: number;
+ /** Child family ID */
+ cid?: number;
+ /** Family's left coordinate */
+ X: number;
+ /** Family's top coordinate */
+ Y: number;
+ parents: readonly Unit[];
+ children: readonly Unit[];
 }
-
+ 
 export type Unit = {
-  /** Family ID */
-  readonly fid: number;
-  /** Is child unit */
-  readonly child: boolean;
-  readonly nodes: readonly Node[];
-  pos: number;
+ /** Family ID */
+ readonly fid: number;
+ /** Is child unit */
+ readonly child: boolean;
+ readonly nodes: readonly Node[];
+ pos: number;
 }
-
+ 
 export type Size = Readonly<{
-  width: number;
-  height: number;
+ width: number;
+ height: number;
 }>
-
+ 
 export type Relation = Readonly<{
-  id: string;
-  type: RelType;
+ id: string;
+ type: RelType;
 }>
-
-export type Node = Readonly<{
-  id: string;
-  gender: Gender;
-  parents: readonly Relation[];
-  children: readonly Relation[];
-  siblings: readonly Relation[];
-  spouses: readonly Relation[];
-  placeholder?: boolean;
+ 
+export type Node = T & Readonly<{
+ id: string;
+ gender: Gender;
+ parents: readonly Relation[];
+ children: readonly Relation[];
+ siblings: readonly Relation[];
+ spouses: readonly Relation[];
+ placeholder?: boolean;
 }>
-
+ 
 export type ExtNode = Node & Readonly<{
-  top: number;
-  left: number;
-  hasSubTree: boolean;
+ top: number;
+ left: number;
+ hasSubTree: boolean;
 }>
-
+ 
 export type Connector = readonly [x1: number, y1: number, x2: number, y2: number];
-
+ 
 export type RelData = Readonly<{
-  canvas: Size;
-  families: readonly Family[];
-  nodes: readonly ExtNode[];
-  connectors: readonly Connector[];
+ canvas: Size;
+ families: readonly Family[];
+ nodes: readonly ExtNode[];
+ connectors: readonly Connector[];
 }>
-
+ 
 export type Options = Readonly<{
-  rootId: string;
-  placeholders?: boolean;
+ rootId: string;
+ placeholders?: boolean;
 }>
