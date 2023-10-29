bd582fd526d0239f3f397d13269c331291f2260e
Denis Novikov <d.novikov@sdventures.com>
2023-10-29 19:41:57 +0300

    feature: add families log

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ export default (nodes: readonly Node[], options: Options): RelData => {
   if (options.placeholders) placeholders(store);
 
   const families = calcFamilies(store).familiesArray;
+  console.log('### families', families)
 
   return {
     families: families,
