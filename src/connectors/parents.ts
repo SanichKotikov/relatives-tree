1e6b810c5bd3372a497f7b830a752681635464ba
Denis Novikov <d.novikov@sdventures.com>
2023-11-05 13:17:35 +0300

    ### 12

--- src/connectors/parents.ts
+++ src/connectors/parents.ts
@@ -12,9 +12,12 @@ const calcConnectors = (family: Family) => (connectors: Connector[], unit: Unit)
   const mY = family.Y + SIZE;
 
   // between parents
-  if (nodeCount(unit) === NODES_IN_COUPLE) connectors.push([pX - HALF_SIZE, pY, pX + HALF_SIZE, pY]);
+  if (nodeCount(unit) === NODES_IN_COUPLE) {
+    console.log('### X parents 1', [pX - HALF_SIZE, pY, pX + HALF_SIZE, pY])
+    connectors.push([pX - HALF_SIZE, pY, pX + HALF_SIZE, pY])
+  };
   // from parent(s) to child
-  console.log('### parents 1', [pX, pY, pX, mY])
+  console.log('### parents 2', [pX, pY, pX, mY])
   connectors.push([pX, pY, pX, mY]);
 
   const child = family.children[0]!;
@@ -22,11 +25,11 @@ const calcConnectors = (family: Family) => (connectors: Connector[], unit: Unit)
   const cX = getUnitX(family, child) + child.nodes.findIndex(withIds(getChildIDs(unit))) * SIZE + HALF_SIZE;
 
   // from child to parent(s)
-  console.log('### parents 2', [cX, mY, cX, mY + HALF_SIZE])
-  // connectors.push([cX, mY, cX, mY + HALF_SIZE]);
+  console.log('### parents 3', [cX, mY, cX, mY + HALF_SIZE])
+  connectors.push([cX, mY, cX, mY + HALF_SIZE]);
   // horizontal between parent(s) and child
   if (pX !== cX) {
-    console.log('### parents 3', [cX, mY, cX, mY + HALF_SIZE])
+    console.log('### parents 4', [cX, mY, cX, mY + HALF_SIZE])
     connectors.push([Math.min(pX, cX), mY, Math.max(pX, cX), mY])
   };
 
